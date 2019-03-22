fbbc54fec9358fd9c42e461fe6191ffae4c95737
Kyler Clegg <kclegg101@hotmail.com>
2019-03-22 01:22:45 -0400

    Updated url patterns to now be able to pass in dict data to steamView html template page.

--- jomon/urls.py
+++ jomon/urls.py
@@ -44,5 +44,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-	url(r'^steamView$', TemplateView.as_view(template_name='steamView.html'), name='home')
+	url(r'^steamView/', include('steam.urls'))
 ]
--- steam/urls.py
+++ steam/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.steamView, name='steamView'),
     url(r'^$', views.index, name='index'),
 ]
