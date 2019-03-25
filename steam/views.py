435777abf94703f2830956649861770281105526
Kyler Clegg <kclegg101@hotmail.com>
2019-03-25 17:12:45 -0400

    Template now displays 5 random curators. Also removed hardcoding for original html placeholders

--- steam/views.py
+++ steam/views.py
@@ -1,39 +1,60 @@
-from django.shortcuts import render
-
-from django.template import loader
-from django.http import HttpResponse
-
+import random
 from . import update
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the Steam index.")
 
 
 def steamView(request):
-	# 1) Call functions getCurators(), getRecommendationsSet(...), 
-	# updateRecommendationCount(...), sortRecommendations(...)
-	# 2) Import html page in this file.
-	# 3) Pass data to html page
-	# 4) On html page, when "follow" has been clicked, add curator to
-	# followed_curators, increment curatorIndex for  and update current_recommendation_set
-	# with their recommended games, and finally for displaying purposes
-	# call sortRecommendations() to get proper order [].
-	
-	#recommendations = {} # Our UNSORTED dict of recommendations
-	#followed_curator_ids = [] # Our array of followed curator ids ('clanID')
-	#curators = update.steam_api.getCurators() # Our dict of all steam curators
-	
-	# consider converting curators to a list()
-	#curatorId = 0
-	#if len(followed_curators) == 0:
-	#	recommendations = update.steam_api.getRecommendationsSet(followed_curators[curatorId])
+	recommendations = {} # Our UNSORTED dict of recommendations
+	temp_recommendations = {}
+	sorted_recommendations = [] # Our SORTED list of recommendations
+	followed_curator_ids = [] # Our array of followed curator ids ('clanID')
+	curators = update.steam_api.getCurators() # Our dict of all steam curators
+	five_random_curators = [] # Our list of 5 random suggested curators to follow
+	random_curators = [] # list of random curators
 
-	temp1 = {'firstname': 'jimbo','lastname': 'bob'}
-	temp2 = "rainy"
+	# Get 5 random curators and append to our list to be passed to 'Suggested Curators'
 
-	data = {
-		'person': temp1,
-		'weather': temp2
-	}
-	return render(request,'../templates/steamView.html',data)
+	rndcur0 = curators[random.choice(list(curators.keys()))]
+	rndcur1 = curators[random.choice(list(curators.keys()))]
+	rndcur2 = curators[random.choice(list(curators.keys()))]
+	rndcur3 = curators[random.choice(list(curators.keys()))]
+	rndcur4 = curators[random.choice(list(curators.keys()))]
+
+	five_random_curators.append(rndcur0)
+	five_random_curators.append(rndcur1)
+	five_random_curators.append(rndcur2)
+	five_random_curators.append(rndcur3)
+	five_random_curators.append(rndcur4)
+
+	print()
+	print(five_random_curators)
+	
+	current_curatorId = 0
+
+	#if 'follow' button clicked:
+		#	get curator from 'curators' dict
+		#	set 'current_curatorId' and add id to 'followed_curator_ids'
+		#	temp_recommendations = update.steam_api.getRecommendationsSet(curators[current_curatorId]) #may need to get .str() for current_curatorId
+
+	"""if len(followed_curator_ids) > 0:
+		recommendations = update.steam_api.updateRecommendationCount(recommendations, temp_recommendations)
+		sorted_recommendations = update.steam_api.sortRecommendations(recommendations)
+	else:
+	 recommendations = temp_recommendations"""
+
+	mylist = [{'id': '346110', 'desc': 'jim is cool'}, {'id': '730', 'desc': 'bob is cooler'}]
+
+	#{%for item in list%}
+	#	<li>{{item.id}}</li>
+	#{% endfor %}
+
+	#return render(request,'../templates/steamView.html',{"list1": sorted_recommendations, "list2": five_random_curators})
+
+	return render(request,'../templates/steamView.html',{"list1": mylist, "list2": five_random_curators})
 
