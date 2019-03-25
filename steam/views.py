5c41c044a2e729958ac411ae7fb7f14a933a4e5e
Kyler Clegg <kclegg101@hotmail.com>
2019-03-25 17:57:56 -0400

    Minor updates to view

--- steam/views.py
+++ steam/views.py
@@ -13,6 +13,7 @@ def steamView(request):
 	recommendations = {} # Our UNSORTED dict of recommendations
 	temp_recommendations = {}
 	sorted_recommendations = [] # Our SORTED list of recommendations
+	
 	followed_curator_ids = [] # Our array of followed curator ids ('clanID')
 	curators = update.steam_api.getCurators() # Our dict of all steam curators
 	five_random_curators = [] # Our list of 5 random suggested curators to follow
@@ -20,23 +21,20 @@ def steamView(request):
 
 	# Get 5 random curators and append to our list to be passed to 'Suggested Curators'
 
-	rndcur0 = curators[random.choice(list(curators.keys()))]
-	rndcur1 = curators[random.choice(list(curators.keys()))]
-	rndcur2 = curators[random.choice(list(curators.keys()))]
-	rndcur3 = curators[random.choice(list(curators.keys()))]
-	rndcur4 = curators[random.choice(list(curators.keys()))]
+	five_random_curators.append(curators[random.choice(list(curators.keys()))])
+	five_random_curators.append(curators[random.choice(list(curators.keys()))])
+	five_random_curators.append(curators[random.choice(list(curators.keys()))])
+	five_random_curators.append(curators[random.choice(list(curators.keys()))])
+	five_random_curators.append(curators[random.choice(list(curators.keys()))])
 
-	five_random_curators.append(rndcur0)
-	five_random_curators.append(rndcur1)
-	five_random_curators.append(rndcur2)
-	five_random_curators.append(rndcur3)
-	five_random_curators.append(rndcur4)
-
-	print()
-	print(five_random_curators)
-	
 	current_curatorId = 0
 
+	#{% for item in list1 %}
+	#	{% if forloop.counter < 5 %}
+	#
+	#	{% endif %}
+	#{% endfor %}
+	
 	#if 'follow' button clicked:
 		#	get curator from 'curators' dict
 		#	set 'current_curatorId' and add id to 'followed_curator_ids'
@@ -49,10 +47,6 @@ def steamView(request):
 	 recommendations = temp_recommendations"""
 
 	mylist = [{'id': '346110', 'desc': 'jim is cool'}, {'id': '730', 'desc': 'bob is cooler'}]
-
-	#{%for item in list%}
-	#	<li>{{item.id}}</li>
-	#{% endfor %}
 
 	#return render(request,'../templates/steamView.html',{"list1": sorted_recommendations, "list2": five_random_curators})
 
